1d934cfe3d1d2c308c9ecaf8811d7a7822c3e1d3
Vic_Ditching <67437207+VicDitching@users.noreply.github.com>
2025-10-23 10:48:49 -0700

    Update PokerPlayer.cpp
    Removed the score functions and score initialization.

--- PokerPlayer.cpp
+++ PokerPlayer.cpp
@@ -3,38 +3,22 @@
 using namespace std;
 
 PokerPlayer::PokerPlayer(const string& name, int initialChips) 
-    : Player(name), score(0), total(0) ,chips(initialChips) {}
+    : Player(name), total(0) ,chips(initialChips) {}
     
 PokerPlayer::~PokerPlayer() {}
 
-void PokerPlayer::setScore(int s){
-    score = s; 
-}
+void PokerPlayer::addWinnings(int amount){ total += amount; }
 
-int PokerPlayer::getScore() const{
-    return score; 
-}
+int PokerPlayer::getTotalWinnings() const{ return total; }
 
-void PokerPlayer::addWinnings(int amount){
-    total += amount; 
-}
+void PokerPlayer::addChips(int amount){ chips += amount; }
 
-int PokerPlayer::getTotalWinnings() const{
-    return total; 
-}
-
-void PokerPlayer::addChips(int amount){
-    chips += amount; 
-}
-
-int PokerPlayer::getChips() const{
-    return chips; 
-}
+int PokerPlayer::getChips() const{ return chips; }
 
 void PokerPlayer::displayHand() const{
     cout<<"[PokerPlayer] " << name <<"'s Hand: "<<endl; 
     for(int x = 0; x < 2; x++){
-        hand[x].displayCard(); 
+        cout<<hand[x]<<endl;  
     }
     cout<<endl<<"Chips: "<<chips<<endl; 
     cout<<"Total Winnings: "<<total<<endl<<endl; 
