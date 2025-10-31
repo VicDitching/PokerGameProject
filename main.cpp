528a301143dd8c60df6e9d7de0ffe718c471b975
Vic_Ditching <67437207+VicDitching@users.noreply.github.com>
2025-10-30 19:52:28 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,35 +3,28 @@
 using namespace std; 
 int main()
 {
-    try{
-        string name; 
-        int startingChips; 
-        // prompts user for their name 
-        cout<<"Enter your name: "; 
-        getline(cin, name); 
-        // prompts user for number of chips that they want to start with 
-        // includes error handling 
-        do{
-            cout<<"Enter the number of chips to start with: "; 
-            cin>>startingChips; 
+    string name; 
+    int startingChips; 
+    // prompts user for their name 
+    cout<<"Enter your name: "; 
+    getline(cin, name); 
+    // prompts user for number of chips that they want to start with 
+    // includes error handling 
+    do{
+        cout<<"Enter the number of chips to start with: "; 
+        cin>>startingChips; 
         
-            if(startingChips <= 0 || !cin){
-                cout<<"Error. Please enter a valid number of chips."<<endl; 
-                cin.clear(); 
-                cin.ignore(1000, '\n');
-            }
-        } while(startingChips <= 0 || !cin); 
-        // initalizes and plays game for user 
-        PokerGame game(name, startingChips); 
-        game.playGame(); 
-        // tells user that their rounds have been recorded onto a text file 
-        cout<<"Session has ended. Please check 'poker_results.txt' for saved results"<<endl; 
-    } catch (const exception&e){ // catches standard exceptions 
-        cerr<<"A critical error occurred during the game."<<e.what()<<endl; 
-        return 1; //indicates an error 
-    } catch (...){ // catches all other unknown exceptions 
-        cerr<<"An unknown, fatal error occured during program executed."<<endl; 
-        return 1; //indicates an error 
-    } 
+        if(startingChips <= 0 || !cin){
+            cout<<"Error. Please enter a valid number of chips."<<endl; 
+            cin.clear(); 
+            cin.ignore(1000, '\n');
+        }
+    } while(startingChips <= 0 || !cin); 
+    // initalizes and plays game for user 
+    PokerGame game(name, startingChips); 
+    game.playGame(); 
+    // tells user that their rounds have been recorded onto a text file 
+    cout<<"Session has ended. Please check 'poker_results.txt' for saved results"<<endl; 
+    
     return 0;
 }
