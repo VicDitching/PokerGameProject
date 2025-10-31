1f597a68ae6dc8e9643fb671fa0985cb6d520e4d
Vic_Ditching <67437207+VicDitching@users.noreply.github.com>
2025-10-30 19:44:54 -0700

    Update PokerPlayer.cpp

--- PokerPlayer.cpp
+++ PokerPlayer.cpp
@@ -1,20 +1,18 @@
-#include <iostream>
 #include "PokerPlayer.h"
-using namespace std;
-
+// constructor which uses intializations 
 PokerPlayer::PokerPlayer(const string& name, int initialChips) 
     : Player(name), total(0) ,chips(initialChips) {}
-    
+// deconstructor 
 PokerPlayer::~PokerPlayer() {}
-
+// adds the winnings user has gained/losed 
 void PokerPlayer::addWinnings(int amount){ total += amount; }
-
+// returns the winnings of the user 
 int PokerPlayer::getTotalWinnings() const{ return total; }
-
+// adds the chips user has gained/losed 
 void PokerPlayer::addChips(int amount){ chips += amount; }
-
+// returns the user's chips 
 int PokerPlayer::getChips() const{ return chips; }
-
+// overrided function where it shows user's hand and chips 
 void PokerPlayer::displayHand() const{
     cout<<"[PokerPlayer] " << name <<"'s Hand: "<<endl; 
     for(int x = 0; x < 2; x++){
