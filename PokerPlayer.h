1bb636e0239311f657cff6bbd3b99fec448efbf4
Vic_Ditching <67437207+VicDitching@users.noreply.github.com>
2025-10-30 19:45:30 -0700

    Update PokerPlayer.h

--- PokerPlayer.h
+++ PokerPlayer.h
@@ -4,22 +4,22 @@
 #include <iostream>
 #include "Player.h"
 using namespace std;
-
+// creates a child class from Player called Poker Player 
 class PokerPlayer : public Player{
     private: 
         int total; 
         int chips; 
     
     public: 
+        // constructor and deconstructor 
         PokerPlayer(const string& name, int initialChips = 50); 
         ~PokerPlayer(); 
-
+        //function prototypes 
         void addWinnings(int amount); 
         int getTotalWinnings() const; 
-        
         void addChips(int amount); 
         int getChips() const; 
-        
+        // override function 
         void displayHand() const override; 
 }; 
 
