97ad667828fe12057950fb7a576187702845df36
Vic_Ditching <67437207+VicDitching@users.noreply.github.com>
2025-10-30 19:42:46 -0700

    Update Card.h

--- Card.h
+++ Card.h
@@ -3,15 +3,16 @@
 
 #include <iostream>
 using namespace std;
-
+// creates class for Card 
 class Card{
     private: 
         string rank; 
         string suit; 
     public: 
+        // default and non-default constructors 
         Card(); 
         Card(string r, string s);
-        
+        // function prototypes 
         string getRank() const;
         string getSuit() const;
         friend ostream& operator<<(ostream& os, const Card& card); 
