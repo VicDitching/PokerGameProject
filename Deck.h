8f513525e8657ee1ed476393b3208db4a09a4d84
Vic_Ditching <67437207+VicDitching@users.noreply.github.com>
2025-10-30 19:43:49 -0700

    Update Deck.h

--- Deck.h
+++ Deck.h
@@ -7,34 +7,32 @@
 #include <algorithm>
 #include <random>
 using namespace std;
-
+// creates a class for Deck 
 class Deck{
     private: 
         string suits[4] = {"Hearts", "Diamonds", "Clubs", "Clovers"}; 
         string ranks[13] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}; 
         Card deck[52]; 
         int topIndex; 
-        
     public:
+        // function prototypes 
         void initializeDeck(); 
         void shuffleDeck(); 
         Card giveCard(); 
-        
         int getRankValue(string rank); 
         void sortRanks(int hand[5]); 
         void countRanks(Card hand[5], int counts[13]); 
-        
-        bool royalFlush(Card hand[5]); //9
-        
-        bool straight(Card hand[5]); //5
-        bool flush(Card hand[5]); //4
-        bool straightFlush(Card hand[5]); //8 
-        
-        bool fourKind(Card hand[5]); //7
-        bool fullHouse(Card hand[5]); //6
-        bool threeKind(Card hand[5]); //3
-        bool twoPair(Card hand[5]); //2
-        bool onePair(Card hand[5]); //1
+        // determines the type of hand that the user has 
+        bool royalFlush(Card hand[5]); 
+        bool straight(Card hand[5]);
+        bool flush(Card hand[5]);
+        bool straightFlush(Card hand[5]);
+        bool fourKind(Card hand[5]); 
+        bool fullHouse(Card hand[5]); 
+        bool threeKind(Card hand[5]); 
+        bool twoPair(Card hand[5]); 
+        bool onePair(Card hand[5]); 
+        // checks the value of the hand 
         int handEvaluator(Card hand[5]); 
 };
 
