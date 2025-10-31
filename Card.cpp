e2a79e722a66749666c0c5a442648ba1c9d358b2
Vic_Ditching <67437207+VicDitching@users.noreply.github.com>
2025-10-30 19:43:05 -0700

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include "Card.h"
 using namespace std;
-
+// initializes card's rank and suit as blank 
 Card::Card() : rank(""), suit("") {}
-
+// initializes card's rank and suit
 Card::Card(string r, string s){
     rank = r; 
     suit = s; 
 }
-
+// returns the rank of the card 
 string Card::getRank() const{ return rank; }
+// returns the suit of the card 
 string Card::getSuit() const{ return suit; }
-
+//overloaded operator 
 ostream& operator<<(ostream& os, const Card& card){
-    os << card.rank << " of " << card.suit; 
+    os << card.rank << " of "<<card.suit; 
     return os; 
 }
