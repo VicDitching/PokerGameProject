5bd943f5ba91ac9dfa8e0f74a734c94d5269170f
Vic_Ditching <67437207+VicDitching@users.noreply.github.com>
2025-10-30 19:44:30 -0700

    Update Player.h

--- Player.h
+++ Player.h
@@ -4,16 +4,17 @@
 #include <iostream>
 #include "Card.h"
 using namespace std;
-
+// creates a class called Player 
 class Player{
     protected: 
         string name; 
         Card hand[5]; 
     
     public: 
+        // constructor and deconstructor 
         Player(const string& n); 
         virtual ~Player(); 
-        
+        // function prototypes 
         void setCard(int index, const Card& c); 
         Card* getHand(); 
         virtual void displayHand() const; 
